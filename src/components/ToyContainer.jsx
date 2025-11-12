e74738bebff51d02ba2e298e9563542fa009ba67
Ben Goddard <goddard.ben04@gmail.com>
2025-11-11 21:20:19 -0700

    Added like button funcionality

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer({ toys, donateToy }) {
+function ToyContainer({ toys, donateToy, updateToy }) {
   return (
     <div id="toy-collection">{toys.map(toy => <ToyCard
       key={toy.id}
       {...toy}
       donateToy={donateToy}
+      updateToy={updateToy}
     />)}</div>
   );
 }
