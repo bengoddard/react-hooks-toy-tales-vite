a0a5e046859bfb8731c24befed604b0d854f85b2
Ben Goddard <goddard.ben04@gmail.com>
2025-11-11 20:50:28 -0700

    Created donate funcionality for toys

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer({ toys }) {
+function ToyContainer({ toys, donateToy }) {
   return (
     <div id="toy-collection">{toys.map(toy => <ToyCard
       key={toy.id}
       {...toy}
+      donateToy={donateToy}
     />)}</div>
   );
 }
